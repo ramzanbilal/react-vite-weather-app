07d2d18c9a80dd578002efdd889c46f5feffcf69
ramzanBilal <ramzanbilal14@gmail.com>
2025-12-24 00:29:40 +0500

    deployment on vercal

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/", // Use "/" for root domain, or "/repo-name/" for GitHub Pages
   server: {
     host: "::",
     port: 8080,
